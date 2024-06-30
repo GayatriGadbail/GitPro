4b7df61c5616acddd14e2da6de7c3e1d29fc30b4
GayatriGadbail <gdgadbail2003@gmail.com>
2024-06-30 13:19:25 +0530

    by gayatriii

--- GitDemo/src/com/Demo.java
+++ GitDemo/src/com/Demo.java
@@ -1,7 +1,9 @@
 package com;
 
 public class Demo {
-	
+	public void m1() {
+		System.out.println("This is a m1 method");
+	}
 		public static void main(String args[]) {
 			System.out.println("This is main method");
 	}
