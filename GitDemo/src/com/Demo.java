f877fd2451c1cfdba30617d6acac0ad4a28239e8
GayatriGadbail <gdgadbail2003@gmail.com>
2024-06-30 13:27:11 +0530

    git Gayatri

--- GitDemo/src/com/Demo.java
+++ GitDemo/src/com/Demo.java
@@ -4,6 +4,9 @@ public class Demo {
 	public void m1() {
 		System.out.println("This is a m1 method");
 	}
+	public void m2(){
+		System.out.println("This is a m2 method");
+	}
 		public static void main(String args[]) {
 			System.out.println("This is main method");
 	}
